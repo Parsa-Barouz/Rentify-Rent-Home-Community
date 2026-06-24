74de924ae05372ca41758c748f25b7ba28921f8c
Parsa-Barouz <parsabarouz@gmail.com>
2026-06-24 16:41:26 +0330

    Update Header

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-
+import Header from './Header'
 function App() {
   return(
 
-    <div className='text-6xl'>iran</div>
+    <div className='container'>
+
+<Header/>
+
+    </div>
 
   )
 
