59ebf392b8abf911615b2d08633e1bb5b73ec374
Parsa-Barouz <parsabarouz@gmail.com>
2026-08-13 16:34:33 +0330

    Update PropertyDetail

--- src/Pages/PropertyDetail.jsx
+++ src/Pages/PropertyDetail.jsx
@@ -1,6 +1,15 @@
 import {React , useEffect, useState} from 'react'
 import { useParams  } from 'react-router-dom'
 import { getProperties } from "../services/propertyService";
+import PropertiesNavbar from '../Properties/PropertiesNavbar';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
+import { Pagination } from 'swiper/modules';
 
 function PropertyDetail() {
 
@@ -11,20 +20,81 @@ useEffect(() => {
 
 
     setProperty(
-      data.find(property => property.id === id)
-    );
+data.find(property => property.id === id)
+
+
+);
   });
 }, []);
 
 
-
-  
 
   return (
 
 
     <>
     
+<div className="container mx-auto w-full max-w-[1500px] px-4 sm:px-6 lg:px-8">
+
+<PropertiesNavbar/>
+
+</div>
+
+
+<div className="section bg-white mt">
+
+<div className="slider">
+
+
+
+      <Swiper
+        slidesPerView={5}
+        spaceBetween={3}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+
+{
+
+property?.images.map((image)=>{
+    console.log("IMAGE:", image);
+return(
+
+
+  <SwiperSlide> 
+          <img src={image} alt="" />
+
+        </SwiperSlide>
+)
+          
+
+
+})
+
+
+}
+
+    
+        {/* <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide> */}
+      </Swiper>
+
+
+</div>
+
+
+
+</div>
+
 
 
 <h1>{property?.title}</h1>    
